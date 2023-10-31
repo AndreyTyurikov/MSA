35a2f8d2206ddc608b20f64cb8052240f93532f4
Andrew_Tiurikov <andrew@realplusonline.com>
2023-10-31 12:27:09 +0100

    Fanout Eachange Type

---
+++ MessageBrokerSamples/FanoutExchange/Consumer/MessageConsumers/MessageConsumers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+string messageQueueName = "first.queue.for.fanout.exchange";
+string fanOutExchangeName = "custom.fanout.exchange";
+
+ConnectionFactory rabbitConnectionFactory = new ConnectionFactory { HostName = "localhost" };
+
+using (IConnection rabbitConnection = rabbitConnectionFactory.CreateConnection())
+{
+    using (IModel channel = rabbitConnection.CreateModel())
+    {
+        //Объявляем очередь самостоятельно
+        channel.QueueDeclare(
+            queue: messageQueueName,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+            );
+
+        //Привязываем вручную очередь к нужному обменнику
+        channel.QueueBind(messageQueueName, fanOutExchangeName, "");
+
+        //Читалка сообщений из канала
+        EventingBasicConsumer channelEventsConsumer = new EventingBasicConsumer(channel);
+
+        channelEventsConsumer.Received += (sender, args) => {
+
+            byte[] binaryMessageBody = args.Body.ToArray();
+
+            string stringMessageBody = Encoding.Unicode.GetString(binaryMessageBody);
+
+            Console.WriteLine($"DeliveryTag #{args.DeliveryTag} received: {stringMessageBody}.");
+            Console.WriteLine($"Exchage: {args.Exchange}. Routing Key (Queue Name): {args.RoutingKey}");
+
+            Thread.Sleep(1000);
+        };
+
+        while (true)
+        {
+            channel.BasicConsume(messageQueueName, autoAck: true, consumer: channelEventsConsumer);
+        }
+
+    }
+}
---
+++ MessageBrokerSamples/FanoutExchange/Producer/MessageProducer/MessageProducer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//TODO: Try to create our own exchnage
+string fanOutExchangeName = "custom.fanout.exchange";
+
+ConnectionFactory rabbitConnectionFactory = new ConnectionFactory { HostName = "localhost" };
+
+using (IConnection rabbitConnection = rabbitConnectionFactory.CreateConnection())
+{
+    using (IModel channel = rabbitConnection.CreateModel())
+    {
+        //Создадим собственный exchange
+        channel.ExchangeDeclare(
+            exchange: fanOutExchangeName,
+            type: ExchangeType.Fanout,
+            durable: true,
+            autoDelete: false,
+            null
+            );
+
+        long messageNumber = 1;
+
+        while (messageNumber < messageNumber + 1000)
+        {
+            string message = $"Message #{messageNumber++}. For any queue of amq.fanout";
+
+            byte[] binaryMessageBody = Encoding.Unicode.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: fanOutExchangeName,
+                routingKey: "", // <- Exchange типа fanout игнорирует RoutingKey
+                mandatory: false,
+                basicProperties: null,
+                body: binaryMessageBody
+                );
+
+            Thread.Sleep(1000);
+        }
+    }
+}
